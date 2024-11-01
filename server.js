2d15f5429cdac01b8fab30ce8dd36ade5e11889c
marlon d c ruz <marlon.cruz@housing.com>
2024-11-01 21:18:14 +0530

    added basic routing and pushed

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 import { createServer } from "http";
-import { readFile } from "fs/promises";
 import { renderJSXToHTML } from "./utils/renderJSX.js";
-import App from "./views/app.js";
 
-createServer(async (_, res) => {
-  const author = "Jae Doe";
-  const postContent = await readFile("./assets/sample.txt", "utf8");
-  sendHTML(
-    res,
-    <html>
-      <head>
-        <title>My blog</title>
-      </head>
-      <body>
-        <App postContent={postContent} author={author} />
-      </body>
-    </html>
-  );
+import App from "./views/layout.js";
+import router from "./router/index.js";
+
+
+createServer(async (req, res) => {
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const page = await router(url);
+    sendHTML(res, <App>{page}</App>);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = err.statusCode ?? 500;
+    res.end();
+  }
 }).listen(8080);
 
 function sendHTML(res, jsx) {
--- views/content.js
+++
@@ -1,5 +0,0 @@
-export default function Content (props) {
-  return (
-    <article>{props.children}</article>
-  )
-}
